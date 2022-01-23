c9125bc1c3f03d74c8639ddb9d59c96ba8ba96fc
Brett Ziegler <brettziegler12@gmail.com>
2022-01-23 18:41:37 -0500

    Temp Convert, Linear Convert, Fibonacci completed

--- module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -1,8 +1,37 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+
+		System.out.print("Please enter a number: ");
+		String numberStr = scanner.nextLine();
+		int number = Integer.parseInt(numberStr);
+
+		int i = 1;
+		int firstTerm = 0;
+		int secondTerm = 1;
+
+		if (number == 0 || number < 0) {
+			System.out.println("0, 1");
+		}
+		else if (number == 1) {
+			System.out.println("0, 1, 1");
+		}
+		else {
+			while (firstTerm <= number) {
+				System.out.print(firstTerm + ", ");
+
+				int nextTerm = firstTerm + secondTerm;
+				firstTerm = secondTerm;
+				secondTerm = nextTerm;
+
+				i++;
+			}
+		}
 
 	}
 
--- module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/LinearConvert.java
@@ -1,8 +1,30 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class LinearConvert {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+
+		System.out.print("Please enter the length: ");
+		String lengthStr = scanner.nextLine();
+		double length = Double.parseDouble(lengthStr);
+
+		System.out.print("Is the measurement in (m)eter, or (f)eet? ");
+		String measurement = scanner.nextLine();
+
+		double meters;
+		double feet;
+
+		if (measurement.equals("f")) {
+			meters = length * 0.3048;
+			System.out.format("%3.2ff is %3.2fm", length, meters);
+		}
+		else if (measurement.equals("m")) {
+			feet = length * 3.2808399;
+			System.out.format("%3.2fm is %3.2ff", length, feet);
+		}
 
 	}
 
