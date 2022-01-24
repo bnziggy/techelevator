869109ccbeba113829b4d651ecb044b00a3b289f
Brett Ziegler <brettziegler12@gmail.com>
2022-01-24 14:48:38 -0500

    Exercise 5 completed

--- module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,8 +1,24 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+
+		System.out.print("Please enter in a series of decimal values (separated by spaces): ");
+		String givenNumberStr = scanner.nextLine();
+		String[] givenNumbers = givenNumberStr.split(" ");
+//		int[] numbers = new int[givenNumbers];
+
+
+		for (int i = 0; i < givenNumbers.length; i++) {
+			int numbers = Integer.parseInt(givenNumbers[i]);
+			System.out.println(numbers + " in binary is " + Integer.toBinaryString(numbers));
+		}
+
+
 
 	}
 
