1bb4e9c20144b15856520077a5a8dc93a198c9ca
Brett Ziegler <brettziegler12@gmail.com>
2022-01-30 00:51:02 -0500

    Exercise 9 Complete

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Elevator.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Elevator.java
@@ -2,4 +2,52 @@ package com.techelevator;
 
 public class Elevator {
 
+    private int currentFloor;
+    private int numberOfFloors;
+    private boolean doorOpen;
+
+    public Elevator(int numberOfLevels) {
+        this.numberOfFloors = numberOfLevels;
+        currentFloor = 1;
+    }
+
+    public void openDoor() {
+        this.doorOpen = true;
+    }
+
+    public void closeDoor() {
+        this.doorOpen = false;
+    }
+
+    public void goUp(int desiredFloor) {
+        if (doorOpen != true) {
+            if (desiredFloor <= numberOfFloors) {
+                if (desiredFloor > currentFloor) {
+                    currentFloor = desiredFloor;
+                }
+            }
+        }
+    }
+
+    public void goDown(int desiredFloor) {
+        if (doorOpen != true) {
+            if (desiredFloor >= 1) {
+                if (desiredFloor < currentFloor) {
+                    currentFloor = desiredFloor;
+                }
+            }
+        }
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public int getNumberOfFloors() {
+        return numberOfFloors;
+    }
+
+    public boolean isDoorOpen() {
+        return doorOpen;
+    }
 }
--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Television.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Television.java
@@ -1,5 +1,82 @@
 package com.techelevator;
 
 public class Television {
+    private boolean isOn;
+    private int currentChannel;
+    private int currentVolume;
 
+    public Television() { // Set default values
+        this.isOn = false;
+        this.currentChannel = 3;
+        this.currentVolume = 2;
+    }
+
+    // Methods to change
+    public void turnOff() {
+        this.isOn = false;
+    }
+
+    public void turnOn() {
+        this.isOn = true;
+        this.currentChannel = 3;
+        this.currentVolume = 2;
+    }
+
+    public void changeChannel(int newChannel) {
+        if (isOn == true && (newChannel >= 3 && newChannel <= 18)) {
+            this.currentChannel = newChannel;
+        }
+    }
+
+    public void channelUp() {
+        if (isOn == true) {
+            if (currentChannel == 18) {
+                currentChannel = 3;
+            }
+            else {
+                this.currentChannel++;
+            }
+        }
+    }
+
+    public void channelDown() {
+        if (isOn == true) {
+            if (currentChannel == 3) {
+                currentChannel = 18;
+            }
+            else {
+                this.currentChannel--;
+            }
+        }
+    }
+
+    public void raiseVolume() {
+        if (isOn == true) {
+            if (currentVolume < 10) {
+                this.currentVolume++;
+            }
+        }
+    }
+
+    public void lowerVolume() {
+        if (isOn == true) {
+            if (currentVolume > 0) {
+                this.currentVolume--;
+            }
+        }
+    }
+
+    // Getters
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public int getCurrentChannel() {
+        return currentChannel;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
 }
