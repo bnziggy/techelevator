fefeb634eb9433cd7888ef013901dec7f7fbf3ee
Brett Ziegler <brettziegler12@gmail.com>
2022-02-21 23:33:06 -0500

    Exercise 4

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/08_remove_person.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/08_remove_person.sql
@@ -1,3 +1,10 @@
 -- 8. Remove "Penn Jillette" from the person table 
--- You'll have to remove data from another table before you can make him "disappear" (Get it? Because he's a magician...) (1 row each)
+-- You'll have to remove data from another table before you can make him "disappear" 
+-- (Get it? Because he's a magician...) (1 row each)
+BEGIN TRANSACTION;
 
+DELETE FROM movie_actor
+WHERE actor_id = 37221;
+
+DELETE FROM person
+WHERE person_id = 37221;
